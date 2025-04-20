87837118a0ce0cd51b89b9bf3aae698796a350db
Aurelisa Juan <aurelisa.sindhu@gmail.com>
2025-04-20 09:34:32 -0700

    updated landing page

--- sentinelai_db/app/page.tsx
+++ sentinelai_db/app/page.tsx
@@ -1,115 +1,115 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
-import { createClient, RealtimeChannel } from "@supabase/supabase-js";
-import MallCanvas from "./_components/mall";
+import {useState, useEffect} from 'react'
+import {createClient, RealtimeChannel} from '@supabase/supabase-js'
+import MallCanvas from './_components/mall'
 
 interface Door {
-  id: number;
-  open: boolean;
+  id: number
+  open: boolean
 }
 
 interface Zone {
-  id: number;
-  status: "ok" | "caution" | "danger";
+  id: number
+  status: 'ok' | 'caution' | 'danger'
 }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || ''
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default function Home() {
-  const [doors, setDoors] = useState<Door[]>([]);
-  const [zones, setZones] = useState<Zone[]>([]);
+  const [doors, setDoors] = useState<Door[]>([])
+  const [zones, setZones] = useState<Zone[]>([])
 
   useEffect(() => {
-    let doorChannel: RealtimeChannel;
-    let zoneChannel: RealtimeChannel;
+    let doorChannel: RealtimeChannel
+    let zoneChannel: RealtimeChannel
 
     const fetchDoors = async () => {
-      const { data, error } = await supabase
-        .from("doors")
-        .select("*")
-        .order("id", { ascending: true });
+      const {data, error} = await supabase
+        .from('doors')
+        .select('*')
+        .order('id', {ascending: true})
       if (error) {
-        console.error("Error fetching doors:", error);
+        console.error('Error fetching doors:', error)
       } else {
-        console.log("Doors fetched:", data);
-        setDoors(data);
+        console.log('Doors fetched:', data)
+        setDoors(data)
       }
-    };
+    }
 
     const fetchZones = async () => {
-      const { data, error } = await supabase
-        .from("zones")
-        .select("*")
-        .order("id", { ascending: true });
+      const {data, error} = await supabase
+        .from('zones')
+        .select('*')
+        .order('id', {ascending: true})
       if (error) {
-        console.error("Error fetching zones:", error);
+        console.error('Error fetching zones:', error)
       } else {
-        console.log("Zones fetched:", data);
-        setZones(data);
+        console.log('Zones fetched:', data)
+        setZones(data)
       }
-    };
+    }
 
     doorChannel = supabase
-      .channel("door-channel")
+      .channel('door-channel')
       .on(
-        "postgres_changes",
+        'postgres_changes',
         {
-          event: "UPDATE",
-          schema: "public",
-          table: "doors",
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'doors',
         },
         (payload) => {
-          console.log("Door update received:", payload.new);
+          console.log('Door update received:', payload.new)
           setDoors((prevDoors) =>
             prevDoors.map((door) =>
               door.id === (payload.new as Door).id
                 ? (payload.new as Door)
-                : door
-            )
-          );
-        }
+                : door,
+            ),
+          )
+        },
       )
-      .subscribe();
+      .subscribe()
 
     zoneChannel = supabase
-      .channel("zone-channel")
+      .channel('zone-channel')
       .on(
-        "postgres_changes",
-        { event: "*", schema: "public", table: "zones" },
+        'postgres_changes',
+        {event: '*', schema: 'public', table: 'zones'},
         (payload) => {
-          console.log("Zone update received:", payload.new);
+          console.log('Zone update received:', payload.new)
           setZones((prevZones) =>
             prevZones.map((zone) =>
               zone.id === (payload.new as Zone).id
                 ? (payload.new as Zone)
-                : zone
-            )
-          );
-        }
+                : zone,
+            ),
+          )
+        },
       )
-      .subscribe();
+      .subscribe()
 
-    fetchDoors();
-    fetchZones();
+    fetchDoors()
+    fetchZones()
 
     return () => {
-      doorChannel.unsubscribe();
-      zoneChannel.unsubscribe();
-    };
-  }, []);
+      doorChannel.unsubscribe()
+      zoneChannel.unsubscribe()
+    }
+  }, [])
 
   return (
-    <div className={"flex flex-col items-center justify-center"}>
+    <div className={'flex flex-col items-center justify-center'}>
       {doors.length !== 0 && zones.length !== 0 ? (
         <>
           <header
-            className={"flex flex-col items-center justify-center h-[100px]"}
+            className={'flex flex-col items-center justify-center h-[100px]'}
           >
-            <h1>Mall 3D</h1>
-            <p>A 3D mall simulation built with React Three Fiber</p>
+            <h1 className="text-7xl font-bold uppercase pt-24">Sentinel AI</h1>
+            <p>Real-Time Public Safety AI. Always Listening. Always Ready.</p>
           </header>
           <MallCanvas doors={doors} zones={zones} />
         </>
@@ -117,5 +117,5 @@ export default function Home() {
         <p>Loading</p>
       )}
     </div>
-  );
+  )
 }

ff4abfa0aa2befac3565f491746e04307d896e89
Ryan <ryyu444@gmail.com>
2025-04-20 10:48:25 -0700

    bug fixes

--- sentinelai_db/app/model/page.tsx
+++ sentinelai_db/app/model/page.tsx
@@ -109,18 +109,68 @@ export default function Home() {
     'StudyStart',
     'HandLocker',
     'Orange Republic',
+    'South Hallway',
+    'North Hallway',
   ];
 
-  const zoneColors = {
-    ok: 'text-green-400',
-    caution: 'text-yellow-400',
-    danger: 'text-red-400',
+  // Hardcoded neighbors for each store
+  const neighbors = {
+    0: [1, 8],
+    1: [0, 8],
+    2: [8],
+    3: [8],
+    4: [7],
+    5: [8, 7, 6],
+    6: [5, 7, 8],
+    7: [4, 5, 6],
+    8: [0, 1, 2, 3, 5, 6],
   };
+
+  function determineZoneColor(status: string, index: number) {
+    // Determine the color based on the status and neighbors
+    if (status === 'ok') {
+      if (
+        neighbors[index as keyof typeof neighbors].some(
+          (neighbor) => zones[neighbor]?.status === 'danger'
+        )
+      ) {
+        return 'text-yellow-400'; // Caution color if any neighbor is in danger
+      }
+      return 'text-green-400';
+    } else if (status === 'caution') {
+      return 'text-yellow-400';
+    } else if (status === 'danger') {
+      return 'text-red-400';
+    } else {
+      // Default color if status is not recognized
+      return 'text-green-400';
+    }
+  }
+
+  function determineZoneStatus(status: string, index: number) {
+    // Determine the status based on the status and neighbors
+    if (status === 'ok') {
+      if (
+        neighbors[index as keyof typeof neighbors].some(
+          (neighbor) => zones[neighbor]?.status === 'danger'
+        )
+      ) {
+        return 'caution';
+      }
+      return 'ok';
+    } else if (status === 'caution') {
+      return 'caution';
+    } else if (status === 'danger') {
+      return 'danger';
+    } else {
+      return 'ok';
+    }
+  }
 
   return (
     <div className={'flex flex-col items-center justify-center relative'}>
-      <div className='absolute top-4 left-4 w-full h-full bg-gradient-to-b from-gray-900 to-gray-800 opacity-50'>
-        <div className='flex flex-col text-white border-2 border-white h-[300px] w-[250px] justify-center items-center'>
+      <div className='absolute top-4 left-4 bg-gradient-to-b from-gray-900 to-gray-800 opacity-50'>
+        <div className='flex flex-col text-white border-2 border-white h-[350px] w-[300px] justify-center items-center'>
           <h1 className='text-center text-2xl font-bold underline'>Status</h1>
           <div className='flex flex-col items-center justify-center h-full'>
             {zones.map((zone, index) => (
@@ -129,8 +179,8 @@ export default function Home() {
                   <span className='mr-2'>
                     {`${zoneNames[index]}:`}{' '}
                     <span
-                      className={`${zoneColors[zone.status]}`}
-                    >{`${zone.status}`}</span>
+                      className={`${determineZoneColor(zone.status, index)}`}
+                    >{`${determineZoneStatus(zone.status, index)}`}</span>
                   </span>
                 )}
               </div>
